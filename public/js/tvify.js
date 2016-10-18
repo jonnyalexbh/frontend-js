fbbbe3eee3d4a5ac7ffadac5c9385ae407c5a431
jonnyalexbh <jonnyalex.bh@gmail.com>
2016-10-18 13:16:35 -0500

    tvify upgrade project and gulp.js

---
+++ public/js/tvify.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+})
+
+/* Add and remove class time */
+
+// var $h1= $('h1');
+//
+// $h1.addClass('danger'); // Add class
+//
+// setTimeout(function () {    // remove class time
+//   $h1.removeClass('danger');
+// },1500)
+
+
+/* Iniciar documento jquery */
+
+// $(document).ready(function(){
+//   alert('ready')
+// })
+
+/* Funcion en variable */
+
+// var alerta = function (mensaje)
+// {
+//   alert(mensaje)
+// }
+// alerta('Hola jonnyalex.bh')
